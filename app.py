d4dc91aff7151422d6d1e1fbfe20545c725b9f71
James-Christensen <109244105+James-Christensen@users.noreply.github.com>
2022-12-18 21:32:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 #all data
 @st.cache
 def get_data():
-    all_data=pd.read_csv(r'bardata.csv.csv')  
+    all_data=pd.read_csv(r'bardata.csv')  
     df= pd.DataFrame(data=all_data)
     return df
 
