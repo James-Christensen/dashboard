be41094a605cc2135bcc98f83670c15cfb4ca244
James-Christensen <109244105+James-Christensen@users.noreply.github.com>
2022-12-26 10:55:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+#import style sheet
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 #all data
 
 def get_data():
