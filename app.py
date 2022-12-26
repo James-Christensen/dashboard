d5f0f3e7c47e21deb4af7e351a7d8525e52651a7
James-Christensen <109244105+James-Christensen@users.noreply.github.com>
2022-12-26 10:35:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -49,61 +49,59 @@ if show_countries:
         options=df_Reg["Country"].unique(),
         default=df_Reg["Country"].unique(),
     )
-bar_data=pd.read_csv(r'bardata.csv')
-barDF=pd.DataFrame(data=bar_data)
+barDF = df
+barDF.sort_values(by="Opportunity Index", ascending=False)
+sortx = "Country"
+bar_D = barDF["Depth of Relationship"]
+newDF = bar_D * 100
+newDF = newDF.astype(str)
+
+barDF["Depth of Relationship"] = newDF.astype(str) + "%"
 
 
-barDF=df
-barDF.sort_values(by='Opportunity Index', ascending=False)
-sortx='Country'  
-bar_D=barDF['Depth of Relationship']
-newDF=(bar_D*100)
-newDF=newDF.astype(str)
-
-barDF['Depth of Relationship']= newDF.astype(str) + '%'
-
-
-if influenceSelect=="All":
+if influenceSelect == "All":
     try:
-        df_selection=df.query("Region ==@newRegion & Country == @newCountry")
+        df_selection = df.query("Region ==@newRegion & Country == @newCountry")
     except:
-        df_selection=df.query("Region ==@newRegion")
-else: 
+        df_selection = df.query("Region ==@newRegion")
+else:
     try:
-        df_selection=df.query("Region ==@newRegion & Country == @newCountry & Influence ==@influenceSelect")
+        df_selection = df.query(
+            "Region ==@newRegion & Country == @newCountry & Influence ==@influenceSelect"
+        )
     except:
-        df_selection=df.query("Region ==@newRegion & Influence ==@influenceSelect")
+        df_selection = df.query("Region ==@newRegion & Influence ==@influenceSelect")
     else:
-        df_selection=df.query("Region ==@newRegion")
+        df_selection = df.query("Region ==@newRegion")
 
-count= len(df_selection.index)
-total=(df_selection['Market Size'].sum()/1000).round(2)
-average = round((df_selection['Opportunity Index'].sum())/count,2)
+count = len(df_selection.index)
+total = (df_selection["Market Size"].sum() / 1000).round(2)
+average = round((df_selection["Opportunity Index"].sum()) / count, 2)
 
 
 if colorSelect == "Country":
     color_select = "Country"
-elif colorSelect =="Region":
-    color_select="Region"
+elif colorSelect == "Region":
+    color_select = "Region"
 else:
-    color_select="Influence"
+    color_select = "Influence"
 
 # st.markdown('---')
 
 if logX:
-    axisValue=True
+    axisValue = True
 else:
-    axisValue=False
+    axisValue = False
 
-if total >=1:
-    market= "Billion"
+if total >= 1:
+    market = "Billion"
 else:
-    total=(total *1000).round(2)
+    total = (total * 1000).round(2)
     market = "Million"
-    
-#KPIS Alt View
-with st.expander('Show Stats',expanded=True):
-    left_col,middle_col,right_col = st.columns(3)
+
+# KPIS Alt View
+with st.expander("Show Stats", expanded=True):
+    left_col, middle_col, right_col = st.columns(3)
     with left_col:
         st.subheader("Number of Countries:")
         st.subheader(f"{count}")
@@ -111,74 +109,95 @@ with st.expander('Show Stats',expanded=True):
         st.subheader("Market Size:")
         st.subheader(f"${total} {market}")
     with right_col:
-        st.subheader('Opportunity Index Average:')
+        st.subheader("Opportunity Index Average:")
         st.subheader(f"{average}")
 
 
-#Bubble Chart
-fig = px.scatter(df_selection, y='Opportunity Index', x='Regulatory Index',size="Market Size", color=color_select,hover_name="Country", size_max=bubbleSize,log_y=axisValue,template ="simple_white")
-if color_select=="Regional Influence":
-    fig = px.scatter(df_selection, y='Opportunity Index', x='Regulatory Index',size="Market Size", color=color_select,hover_name="Country", color_discrete_map={"High": "orange","Medium": "yellow","Low": "purple"},category_orders={"Regional Influence":["Low","Medium","High"]},size_max=bubbleSize,log_y=axisValue,template ="simple_white")
+# Bubble Chart
+fig = px.scatter(
+    df_selection,
+    y="Opportunity Index",
+    x="Regulatory Index",
+    size="Market Size",
+    color=color_select,
+    hover_name="Country",
+    size_max=bubbleSize,
+    log_y=axisValue,
+    template="simple_white",
+)
+if color_select == "Regional Influence":
+    fig = px.scatter(
+        df_selection,
+        y="Opportunity Index",
+        x="Regulatory Index",
+        size="Market Size",
+        color=color_select,
+        hover_name="Country",
+        color_discrete_map={"High": "orange", "Medium": "yellow", "Low": "purple"},
+        category_orders={"Regional Influence": ["Low", "Medium", "High"]},
+        size_max=bubbleSize,
+        log_y=axisValue,
+        template="simple_white",
+    )
 
-fig.update_layout(height=700,
+fig.update_layout(
+    height=700,
     title="     Market Overview",
     font=dict(
         family="'Noto Sans KR', sans-serif",
         size=32,
         # color="#F79E1B"
-    ))
+    ),
+)
 
 if tick_values:
     fig.update_layout(
-    xaxis = dict(
-        tickmode = 'array',
-        tickvals = [2,5,8],
-        ticktext = ['Low', 'Med.', 'High']
-    )
+        xaxis=dict(
+            tickmode="array", tickvals=[2, 5, 8], ticktext=["Low", "Med.", "High"]
+        )
     )
 
     fig.update_layout(
-    yaxis = dict(
-        tickmode = 'array',
-        tickvals = [1,2,3],
-        ticktext = ['Low', 'Med.', 'High']
-    )
+        yaxis=dict(
+            tickmode="array", tickvals=[1, 2, 3], ticktext=["Low", "Med.", "High"]
+        )
     )
 else:
     pass
 
-st.plotly_chart(fig,theme="streamlit", height=700, use_container_width=True)
+st.plotly_chart(fig, theme="streamlit", height=700, use_container_width=True)
 
 
+# DataFrame
+with st.expander("Show Data Table", expanded=False):
+    st.dataframe(df_selection, use_container_width=True)
 
-    
-#DataFrame
-with st.expander("Show Data Table", expanded=False): 
-    st.dataframe(df_selection,use_container_width=True)
-    
-#BarChart
-change_sort=st.checkbox(label="Change Sort", value=False, key="ChangeSort")
+# BarChart
+change_sort = st.checkbox(label="Change Sort", value=False, key="ChangeSort")
 
 if change_sort:
-         sortx=alt.X('Country', sort="y")
-        
-bars = alt.Chart(df_selection).mark_bar().encode(
-    x=sortx,
-    y='Opportunity Index',
-    tooltip=[{"field": "Country", "type": "nominal"},
-    {"field": "Count of Aggregators", "type": "quantitative"},
-    {"field": "Fintech / Bank Ratio", "type": "quantitative"},
-    {"field": 'Depth', "type": "nominal", "title": "MA Depth of Relationship"}]
-).configure_mark(
-    color='orange'
+    sortx = alt.X("Country", sort="y")
+
+bars = (
+    alt.Chart(df_selection)
+    .mark_bar()
+    .encode(
+        x=sortx,
+        y="Opportunity Index",
+        tooltip=[
+            {"field": "Country", "type": "nominal"},
+            {"field": "Count of Aggregators", "type": "quantitative"},
+            {"field": "Fintech / Bank Ratio", "type": "quantitative"},
+            {"field": "Depth", "type": "nominal", "title": "MA Depth of Relationship"},
+        ],
+    )
+    .configure_mark(color="orange")
 )
 text = bars.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
-).encode(
-    text='Country'
-)  
+    align="left",
+    baseline="middle",
+    dx=3,  # Nudges text to right so it doesn't appear on top of the bar
+).encode(text="Country")
 st.altair_chart(bars, use_container_width=True)
 
 
