db6b7914e6fcf6047038a524d67eae3e1faf3816
James-Christensen <109244105+James-Christensen@users.noreply.github.com>
2023-01-03 12:24:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ else:
     else:
         df_selection = df.query("Region ==@newRegion")
         
-df_selection = df_selection.round(0)
+df_selection = df_selection.round(2)
 
 count = len(df_selection.index)
 total = (df_selection["Market Size"].sum() / 1000).round(2)
